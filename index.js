8e2c147c7d25210363045926ff0e990410497b79
Sharon Byegon <byegon.sharon@gmail.com>
2024-03-27 06:53:24 +0300

    completed the assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution here
+//function takes an array of names and a string and returns the matching list that is case-sensitive
+function findMatching(drivers, name) {
+    return drivers.filter(function (matchList){
+        return matchList.toLowerCase() === name.toLowerCase();
+    })
+}
+
+//returns all drivers whose names begin with the provided letters
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(
+      (match) =>
+        match.toLowerCase().indexOf(letters.toLowerCase()) === 0 );
+    }
+
+
+//returns the driver whose name matches the provided name
+function matchName(drivers,name){
+    return drivers.filter((match) => {
+        return match.name === name
+    })
+}
